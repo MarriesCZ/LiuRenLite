3773f600a675c88dae903080291187ba24ab53d6
dexiang.deng <dexiang.deng@outlook.com>
2023-10-22 17:35:02 +0800

    引入ViewBind优化代码

--- app/src/main/java/com/marries/liurenlite/MainActivity.kt
+++ app/src/main/java/com/marries/liurenlite/MainActivity.kt
@@ -10,9 +10,9 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.WindowManager
 import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.graphics.toArgb
+import com.marries.liurenlite.databinding.MainLayoutBinding
 import com.marries.liurenlite.liuren.LiuRenLite
 import com.marries.liurenlite.liuren.currentGUA
 import com.marries.liurenlite.ui.theme.ColorTheme
@@ -22,15 +22,11 @@ import kotlin.properties.Delegates
 class MainActivity : AppCompatActivity() {
 
     private val liuRenLite = LiuRenLite()
-    private lateinit var mGuaText: TextView
-    private lateinit var mTimeYin: TextView
-    private lateinit var mDuanText: TextView
-    private lateinit var mDayAndHourText: TextView
-    private lateinit var mGuaMHDText: TextView
+    private lateinit var mViewBinding: MainLayoutBinding
     private var mScreenHeight by Delegates.notNull<Float>()
     private var mScreenWidth by Delegates.notNull<Float>()
-    private val mGuaTextX by lazy { mGuaText.x }
-    private val mGuaTextY by lazy { mGuaText.y }
+    private val guaTextX by lazy { mViewBinding.guaText.x }
+    private val guaTextY by lazy { mViewBinding.guaText.y }
     private var viewState = ViewState.DOWN
 
     enum class ViewState {
@@ -40,77 +36,86 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_layout)
+        mViewBinding = MainLayoutBinding.inflate(layoutInflater)
+        setContentView(mViewBinding.root)
         currentGUA = liuRenLite.getGua()
         val themeColor = ColorTheme.getColorTheme(currentGUA).getThemeColor()
         updateBackground(themeColor)
-        mGuaText = findViewById<TextView>(R.id.gua_text).apply { text = currentGUA.title }
-        mTimeYin = findViewById<TextView>(R.id.time_yin_text).apply { text = liuRenLite.getTimeYin() }
-        mDuanText = findViewById<TextView>(R.id.duan_text).apply { text = currentGUA.duan }
-        mDayAndHourText = findViewById<TextView>(R.id.day_and_hour_text).apply { text = liuRenLite.getGuaWithDayAndHour() }
-        mGuaMHDText = findViewById<TextView>(R.id.month_day_hour_gua_text).apply { text = liuRenLite.getMHD() }
-        initState()
-        findViewById<RelativeLayout>(R.id.content).setOnClickListener {
-            // 是否需要跳过MHD阶段
-            val isDayAndHourEmpty = mDayAndHourText.text.isEmpty()
-            when (viewState) {
-                ViewState.UP -> downSlidAnimator(isDayAndHourEmpty)
-                ViewState.ANIMATE -> Unit
-                ViewState.DOWN -> if (isDayAndHourEmpty) upSlidAnimator(true) else updateAnimator()
-                ViewState.MHD -> upSlidAnimator(false)
+        with(mViewBinding) {
+            guaText.text = currentGUA.title
+            timeYinText.text = liuRenLite.getTimeYin()
+            duanText.text = currentGUA.duan
+            dayAndHourText.text = liuRenLite.getGuaWithDayAndHour()
+            monthDayHourGuaText.text = liuRenLite.getMHD()
+            initState()
+            findViewById<RelativeLayout>(R.id.content).setOnClickListener {
+                // 是否需要跳过MHD阶段
+                val isDayAndHourEmpty = dayAndHourText.text.isEmpty()
+                when (viewState) {
+                    ViewState.UP -> downSlidAnimator(isDayAndHourEmpty)
+                    ViewState.ANIMATE -> Unit
+                    ViewState.DOWN -> if (isDayAndHourEmpty) upSlidAnimator(true) else updateAnimator()
+                    ViewState.MHD -> upSlidAnimator(false)
+                }
             }
         }
     }
 
     private fun initState() {
-        val dm = DisplayMetrics()
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        windowManager.defaultDisplay.getMetrics(dm)
-        mScreenHeight = dm.heightPixels.toFloat()
-        mScreenWidth = dm.widthPixels.toFloat()
+        DisplayMetrics().apply {
+            val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            windowManager.defaultDisplay.getMetrics(this)
+        }.also {
+            mScreenHeight = it.heightPixels.toFloat()
+            mScreenWidth = it.widthPixels.toFloat()
+        }
     }
 
     private fun updateBackground(themeColor: ThemeColor) {
-        window.statusBarColor = themeColor.background.toArgb()
-        window.navigationBarColor = themeColor.background.toArgb()
-        window.setBackgroundDrawable(ColorDrawable(themeColor.background.toArgb()))
+        window.apply {
+            statusBarColor = themeColor.background.toArgb()
+            navigationBarColor = themeColor.background.toArgb()
+            setBackgroundDrawable(ColorDrawable(themeColor.background.toArgb()))
+        }
     }
 
     private fun upSlidAnimator(isDayAndHourEmpty: Boolean) {
-        val lengthX = mScreenWidth - mGuaTextX - mGuaText.width - 2 * mTimeYin.width
-        ValueAnimator.ofFloat(0f, 1.0f).apply {
-            duration = 600
-            repeatCount = 0
-            addUpdateListener {
-                val value = it.animatedValue as Float
-                mGuaText.x = mGuaTextX + value * lengthX
-                mGuaText.y = (1 - value) * mGuaTextY
-                mTimeYin.x = mGuaText.x + mGuaText.width
-                mTimeYin.y = mGuaText.y + mGuaText.height / 2 - mTimeYin.height / 2
-                mTimeYin.alpha = value
-                if (!isDayAndHourEmpty) {
-                    mGuaMHDText.alpha = 1.0f - value
-                    mDayAndHourText.alpha = 1.0f - value
+        with(mViewBinding) {
+            val lengthX = mScreenWidth - guaTextX - guaText.width - 2 * timeYinText.width
+            ValueAnimator.ofFloat(0f, 1.0f).apply {
+                duration = 600
+                repeatCount = 0
+                addUpdateListener {
+                    val value = it.animatedValue as Float
+                    guaText.x = guaTextX + value * lengthX
+                    guaText.y = (1 - value) * guaTextY
+                    mViewBinding.timeYinText.x = guaText.x + guaText.width
+                    mViewBinding.timeYinText.y = guaText.y + guaText.height / 2 - timeYinText.height / 2
+                    mViewBinding.timeYinText.alpha = value
+                    if (!isDayAndHourEmpty) {
+                        monthDayHourGuaText.alpha = 1.0f - value
+                        dayAndHourText.alpha = 1.0f - value
+                    }
+                    duanText.alpha = value
+                    duanText.y = mScreenHeight - duanText.height * (1 + 1.5f * value)
                 }
-                mDuanText.alpha = value
-                mDuanText.y = mScreenHeight - mDuanText.height * (1 + 1.5f * value)
+                addListener(object : AnimatorListener {
+                    override fun onAnimationStart(animation: Animator) {
+                        viewState = ViewState.ANIMATE
+                    }
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        viewState = ViewState.UP
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                    }
+
+                    override fun onAnimationRepeat(animation: Animator) {
+                    }
+                })
+                start()
             }
-            addListener(object : AnimatorListener {
-                override fun onAnimationStart(animation: Animator) {
-                    viewState = ViewState.ANIMATE
-                }
-
-                override fun onAnimationEnd(animation: Animator) {
-                    viewState = ViewState.UP
-                }
-
-                override fun onAnimationCancel(animation: Animator) {
-                }
-
-                override fun onAnimationRepeat(animation: Animator) {
-                }
-            })
-            start()
         }
     }
 
@@ -120,9 +125,9 @@ class MainActivity : AppCompatActivity() {
             repeatCount = 0
             addUpdateListener {
                 val value = it.animatedValue as Float
-                mGuaMHDText.alpha = value
-                mDayAndHourText.alpha = value
-                mDayAndHourText.y = mScreenHeight - mDayAndHourText.height * (1 + 1.5f * value)
+                mViewBinding.monthDayHourGuaText.alpha = value
+                mViewBinding.dayAndHourText.alpha = value
+                mViewBinding.dayAndHourText.y = mScreenHeight - mViewBinding.dayAndHourText.height * (1 + 1.5f * value)
             }
             addListener(object : AnimatorListener {
                 override fun onAnimationStart(animation: Animator) {
@@ -144,21 +149,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun downSlidAnimator(isDayAndHourEmpty: Boolean) {
-        val lengthX = mScreenWidth - mGuaTextX - mGuaText.width - 2 * mTimeYin.width
+        val lengthX = mScreenWidth - guaTextX - mViewBinding.guaText.width - 2 * mViewBinding.timeYinText.width
         ValueAnimator.ofFloat(1.0f, 0f).apply {
             duration = 600
             repeatCount = 0
             addUpdateListener {
-                val value = it.animatedValue as Float
-                mGuaText.x = mGuaTextX + value * lengthX
-                mGuaText.y = (1 - value) * mGuaTextY
-                mTimeYin.x = mGuaText.x + mGuaText.width
-                mTimeYin.y = mGuaText.y + mGuaText.height / 2 - mTimeYin.height / 2
-                mTimeYin.alpha = value
-                mDuanText.alpha = value
-                mDuanText.y = mScreenHeight - mDuanText.height * (1 + 1.5f * value)
-                if (!isDayAndHourEmpty) {
-                    mDayAndHourText.y = mScreenHeight - mDayAndHourText.height * (1 + 1.5f * value)
+                with(mViewBinding) {
+                    val value = it.animatedValue as Float
+                    guaText.x = guaTextX + value * lengthX
+                    guaText.y = (1 - value) * guaTextY
+                    timeYinText.x = guaText.x + guaText.width
+                    timeYinText.y = guaText.y + guaText.height / 2 - timeYinText.height / 2
+                    timeYinText.alpha = value
+                    duanText.alpha = value
+                    duanText.y = mScreenHeight - duanText.height * (1 + 1.5f * value)
+                    if (!isDayAndHourEmpty) {
+                        dayAndHourText.y = mScreenHeight - dayAndHourText.height * (1 + 1.5f * value)
+                    }
                 }
             }
             addListener(object : AnimatorListener {
